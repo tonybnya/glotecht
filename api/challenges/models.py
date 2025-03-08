634e45738b8d73d3365000b33190fce4a0234c6f
tonybnya <nya.tony2010@gmail.com>
2025-03-08 13:05:57 +0100

    perf: add src field to the model of a challenge

--- api/challenges/models.py
+++ api/challenges/models.py
@@ -9,6 +9,7 @@ class Challenge(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     tags = models.JSONField(default=list)
+    src = models.CharField(max_length=100, null=True, blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
