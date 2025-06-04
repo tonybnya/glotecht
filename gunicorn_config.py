674cfa483e1fa92017a984faf018713f8105e8dd
tonybnya <nya.tony2010@gmail.com>
2025-06-04 13:47:10 +0100

    ci: prepare for deployment on render

---
+++ gunicorn_config.py
@@ -0,0 +1,24 @@
+import multiprocessing
+
+# Server socket
+bind = "0.0.0.0:10000"
+backlog = 2048
+
+# Worker processes
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_class = 'sync'
+worker_connections = 1000
+timeout = 30
+keepalive = 2
+
+# Logging
+accesslog = '-'
+errorlog = '-'
+loglevel = 'info'
+
+# Process naming
+proc_name = 'glotecht'
+
+# SSL
+keyfile = None
+certfile = None 
